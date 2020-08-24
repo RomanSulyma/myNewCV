b69d16341508cb8e46c9b24cbaf1506ad70140d4
Roman <roman.sulyma97@gmail.com>
2020-08-24 18:12:37 +0300

    fix v2

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ function App() {
     <div className="App">
 
       <div className="row">
-        <a href="https://www.linkedin.com/in/sulima-roman/" target="_blank"><img src={Photo}/></a>
+        <a href="https://www.linkedin.com/in/sulima-roman/" target="_blank" rel="noopener noreferrer">
+            <img src={Photo} alt={'No photo downloaded :('}/>
+        </a>
       </div>
 
       <div className="row">
         <ul>
-          <li><a href="https://drive.google.com/open?id=1UWD1A-hz8JOIEsqzKoZfkACWNkFhO3WC" target="_blank">CV</a></li>
+          <li><a href="https://drive.google.com/file/d/1CMIpfOFO1AsSgSOPMOMs51p-12iXfNBl/view?usp=sharing" target="_blank" rel="noopener noreferrer">CV</a></li>
         </ul>
       </div>
 
@@ -21,20 +23,17 @@ function App() {
 
       <div className="row">
         <ul>
-          <li><a href="https://github.com/RomanSulyma/MyShop" target="_blank">MyShop project</a></li>
-          <li><a href="https://github.com/RomanSulyma/image-resizer" target="_blank">Image resizer project (Spring)</a>
-          </li>
-          <li><a href="https://github.com/RomanSulyma/MyResume" target="_blank">Social network project (Spring,
-            Hibernate)</a></li>
-          <li><a href="https://github.com/RomanSulyma/BudgetPlannerES6" target="_blank">Budget planner project (JS,
-            ES6)</a></li>
-          <li><a href="https://github.com/RomanSulyma/CocktailSearch" target="_blank">Cocktail search project (JS,
-            ES6)</a></li>
-          <li><a href="https://github.com/RomanSulyma/cocktail_project_design" target="_blank">Cocktail company design
-            project (hTML5, CSS3)</a></li>
+          <li><a href="https://github.com/RomanSulyma/MyShop" target="_blank" rel="noopener noreferrer">MyShop project</a></li>
+          <li><a href="https://github.com/RomanSulyma/image-resizer" target="_blank" rel="noopener noreferrer">Image resizer project (Spring)</a></li>
+          <li><a href="https://github.com/RomanSulyma/MyResume" target="_blank" rel="noopener noreferrer">Social network project (Spring, Hibernate)</a></li>
+          <li><a href="https://github.com/RomanSulyma/BudgetPlannerES6" target="_blank" rel="noopener noreferrer">Budget planner project (JS, ES6)</a></li>
+          <li><a href="https://github.com/RomanSulyma/CocktailSearch" target="_blank" rel="noopener noreferrer">Cocktail search project (JS, ES6)</a></li>
+          <li><a href="https://github.com/RomanSulyma/cocktail_project_design" target="_blank" rel="noopener noreferrer">Cocktail company design project (HTML5, CSS3)</a></li>
+          <li><a href="https://github.com/RomanSulyma/burger_builder" target="_blank" rel="noopener noreferrer">Burger builder project front-end (React, ES6)</a></li>
+          <li><a href="https://github.com/RomanSulyma/burger-builder-back-end" target="_blank" rel="noopener noreferrer">Burger builder project back-end (Spring)</a></li>
+          <li><a className={'deployed'} href="http://18.156.3.212:5000" target="_blank" rel="noopener noreferrer">Burger builder project deployed</a></li>
         </ul>
       </div>
-
     </div>
   );
 }
